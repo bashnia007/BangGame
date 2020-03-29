d7d290d94b197cbdc9a4c14e17169539ec5897d6
warrior_spb <ivanovandrew2004@gmail.com>
2020-03-29 13:34:43 +0300

    Create cards #6

--- Domain/PlayingCards/PlayingCard.cs
+++ Domain/PlayingCards/PlayingCard.cs
@@ -2,6 +2,8 @@
 {
     public abstract class PlayingCard : ValueObject<PlayingCard>
     {
+        public Suite Suite { get; set; } 
+        public Rank Rank { get; set; } 
         public abstract bool PlayAndDiscard { get; }
     }
 }
