1686ced0ecc99f2532fd2fd9702c9310ceddce14
warrior_spb <ivanovandrew2004@gmail.com>
2020-04-16 00:28:57 +0300

    Fix problem with cards comparison #51
    Fix generating of card rank

--- Domain/Game/CardFactory.cs
+++ Domain/Game/CardFactory.cs
@@ -5,12 +5,12 @@ namespace Domain.Game
 {
     internal static class CardFactory
     {
-        private static Random Random = new Random();
+        private static readonly Random Random = new Random();
 
         public static BangGameCard Create(Type card)
         {
             var suite = (Suite) Random.Next(0, 4);
-            var rank = (Rank) Random.Next(0, 4);
+            var rank = (Rank) Random.Next(0, 13);
             
             return new BangGameCard((CardType)Activator.CreateInstance(card), suite, rank);
         }
