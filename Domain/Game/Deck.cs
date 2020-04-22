cac29826e002841bf31c166301dcfa500562b458
warrior_spb <ivanovandrew2004@gmail.com>
2020-04-23 00:06:59 +0300

    Shuffle deck after resetting #61
    Reimplement deck implementation
    1) Deck is more like stack than queue
    2) Discard pile is also deck.

--- Domain/Game/Deck.cs
+++ Domain/Game/Deck.cs
@@ -5,36 +5,50 @@ using System.Linq;
 namespace Domain.Game
 {
     [Serializable]
-    public class Deck<T> : Queue<T>
+    public class Deck<T>
     {
+        private Stack<T> cards;
         #region Constructors
 
-        public Deck() : base()
-        { }
-
-        public Deck(IEnumerable<T> data) : base(data)
-        { }
+        
+        public Deck(IEnumerable<T> data)
+        {
+            cards = new Stack<T>(data);
+        }
+        
+        public Deck() : this(new List<T>()){}
 
         #endregion
 
         public Deck<T> Shuffle()
         {
-            var list = this.ToList();
+            var list = cards.ToList();
             int cardsAmount = list.Count;
             var rnd = new Random();
 
-            this.Clear();
+            cards = new Stack<T>();
 
             while (cardsAmount > 0)
             {
                 int number = rnd.Next(cardsAmount);
                 var cardByNumber = list[number];
-                this.Enqueue(cardByNumber);
+                cards.Push(cardByNumber);
                 list.RemoveAt(number);
                 cardsAmount--;
             }
 
             return this;
         }
+
+        public bool IsEmpty() => cards.Count == 0;
+
+        public T Deal()
+        {
+            if (IsEmpty()) throw new InvalidOperationException();
+
+            return cards.Pop();
+        }
+
+        public void Put(T card) => cards.Push(card);
     }
 }
