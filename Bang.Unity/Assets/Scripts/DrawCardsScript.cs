8261699a1c60528d127fcfb4ffa8a186a6643d33
bashnia007 <alexanderkavokin@gmail.com>
2020-05-25 23:54:34 +0200

    added 2 player zones on template scene. Added DragDrop script

---
+++ Bang.Unity/Assets/Scripts/DrawCardsScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCardsScript : MonoBehaviour 
+{
+	public GameObject Card1;
+	public GameObject Card2;
+	public GameObject PlayerArea;
+	public GameObject EnemyArea;
+
+	List<GameObject> cards = new List<GameObject>();
+
+	void Start() 
+	{
+		cards.Add(Card1);
+		cards.Add(Card2);
+	}
+
+	public void OnClick()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+			playerCard.transform.SetParent(PlayerArea.transform, false);
+
+			GameObject enemyCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+			enemyCard.transform.SetParent(EnemyArea.transform, false);
+		}
+	}
+}
