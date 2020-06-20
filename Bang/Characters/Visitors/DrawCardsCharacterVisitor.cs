33443369212bf71dd70185c9287b9fb024f4a6dd
bashnia007 <alexanderkavokin@gmail.com>
2020-06-20 20:46:58 +0200

    added default action for ICharacterVisitor

--- Bang/Characters/Visitors/DrawCardsCharacterVisitor.cs
+++ Bang/Characters/Visitors/DrawCardsCharacterVisitor.cs
@@ -6,10 +6,7 @@ namespace Bang.Characters.Visitors
 {
     public class DrawCardsCharacterVisitor : ICharacterVisitor<Action<Game.Gameplay, Player>>
     {
-        public Action<Game.Gameplay, Player> Visit(BartCassidy character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
+        public Action<Game.Gameplay, Player> DefaultValue => (gameplay, player) => ProvideTwoCards(gameplay, player);
 
         public Action<Game.Gameplay, Player> Visit(BlackJack character)
         {
@@ -24,76 +21,6 @@ namespace Bang.Characters.Visitors
                 }
                 player.AddCardToHand(gameplay.DealCard());
             };
-        }
-
-        public Action<Game.Gameplay, Player> Visit(CalamityJanet character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(ElGringo character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(JessyJones character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(Jourdonnais character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(KitCarlson character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(LuckyDuke character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(PaulRegret character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(PedroRamirez character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(RoseDoolan character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(SidKetchum character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(SlabTheKiller character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(SuzyLafayette character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(VultureSam character)
-        {
-            return (gameplay, player) => ProvideTwoCards(gameplay, player);
-        }
-
-        public Action<Game.Gameplay, Player> Visit(WillyTheKid character)
-        {
-            throw new NotImplementedException();
         }
 
         private void ProvideTwoCards(Game.Gameplay gameplay, Player player)
--- Bang/Characters/Visitors/HasBarrelByDefaultVisitor.cs
+++ Bang/Characters/Visitors/HasBarrelByDefaultVisitor.cs
@@ -1,24 +1,9 @@
-using Bang.Characters;
-
 namespace Bang.Characters.Visitors
 {
     public class HasBarrelByDefaultVisitor : ICharacterVisitor<bool>
     {
-        public bool Visit(BartCassidy character) => false;
-        public bool Visit(BlackJack character) => false;
-        public bool Visit(CalamityJanet character) => false;
-        public bool Visit(ElGringo character) => false;
-        public bool Visit(JessyJones character) => false;
+        public bool DefaultValue => false;
+
         public bool Visit(Jourdonnais character) => true;
-        public bool Visit(KitCarlson character) => false;
-        public bool Visit(LuckyDuke character) => false;
-        public bool Visit(PaulRegret character) => false;
-        public bool Visit(PedroRamirez character) => false;
-        public bool Visit(RoseDoolan character) => false;
-        public bool Visit(SidKetchum character) => false;
-        public bool Visit(SlabTheKiller character) => false;
-        public bool Visit(SuzyLafayette character) => false;
-        public bool Visit(VultureSam character) => false;
-        public bool Visit(WillyTheKid character) => false;
     }
 }
