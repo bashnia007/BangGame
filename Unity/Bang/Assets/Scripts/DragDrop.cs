74bf1ca90c2744aeb5e3a160913c5ce7f054162d
bashnia007 <alexanderkavokin@gmail.com>
2020-09-07 22:20:40 +0200

    added initial UI

---
+++ Unity/Bang/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropZone;
+    private Vector2 startPosition;
+
+    private Queue<GameObject> dropZones = new Queue<GameObject>();
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+        if (!dropZones.Contains(dropZone))
+        {
+            dropZones.Enqueue(dropZone);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (dropZones.Contains(collision.gameObject))
+        {
+            dropZones.Dequeue();
+        }
+        if (dropZones.Count > 0)
+        {
+            dropZone = dropZones.Peek();
+        }
+        else 
+        { 
+            dropZone = null;
+            isOverDropZone = false;
+        }
+    }
+}
---
+++ Unity/Bang/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInitializer : MonoBehaviour
+{
+    public GameObject Tablet;
+    public GameObject Card;
+
+    private const int LivePoints = 5;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        var handArea = Tablet.transform.Find("HandCardsArea");
+
+        var characters = new List<string>
+        {
+            "Images/Characters/pedro_ramirez",
+        };
+        var roles = new List<string>
+        {
+            "Images/Roles/sheriff"
+        };
+        var playingCards = new List<string>
+        {
+            "Images/PlayingCards/Bang_1",
+            "Images/PlayingCards/Bang_2",
+            "Images/PlayingCards/Bang_3",
+            "Images/PlayingCards/barrel",
+            "Images/PlayingCards/Dynamit",
+            "Images/PlayingCards/Gatling",
+            "Images/PlayingCards/Mustang",
+            "Images/PlayingCards/Remington",
+            "Images/PlayingCards/Saloon",
+            "Images/PlayingCards/Scope",
+        };
+
+        var role = Tablet.transform.Find("RoleCard");
+        var spriteName = roles[Random.Range(0, roles.Count)];
+        role.GetComponent<Image>().sprite = Resources.Load<Sprite>(spriteName);
+
+        var characterCard = Tablet.transform.Find("CharacterCard");
+        spriteName = characters[Random.Range(0, characters.Count)];
+        characterCard.GetComponent<Image>().sprite = Resources.Load<Sprite>(spriteName);
+
+        for (int i = 0; i < LivePoints; i++)
+        {
+            GameObject handCard = Instantiate(Card, new Vector3(0, 0, 0), Quaternion.identity);
+            handCard.transform.SetParent(handArea.transform, false);
+            spriteName = playingCards[Random.Range(0, playingCards.Count)];
+            handCard.GetComponent<Image>().sprite = Resources.Load<Sprite>(spriteName);
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
